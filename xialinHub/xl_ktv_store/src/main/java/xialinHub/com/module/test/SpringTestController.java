d3ae935cc9dcdff345ecb0bf9ef9764d82a2b4f0
xialinHub <18323788643@163.com>
2017-05-12 15:00:24 +0800

    add by xialin 20170512

---
+++ xialinHub/xl_ktv_store/src/main/java/xialinHub/com/module/test/SpringTestController.java
@@ -0,0 +1,16 @@
+package xialinHub.com.module.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+ *作者:xialin
+ *日期:2017年5月12日
+ *时间:下午2:56:57
+ **/
+@Controller
+@RequestMapping("springtest")
+public class SpringTestController {
+
+	
+}
