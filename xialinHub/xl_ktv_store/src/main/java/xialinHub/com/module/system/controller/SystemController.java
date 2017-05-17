6560b54e1ac1af7bc185269a93c1f5a75261480f
xialinHub <18323788643@163.com>
2017-05-17 15:15:18 +0800

    add by xialin 20170517

--- xialinHub/xl_ktv_store/src/main/java/xialinHub/com/module/system/controller/SystemController.java
+++ xialinHub/xl_ktv_store/src/main/java/xialinHub/com/module/system/controller/SystemController.java
@@ -1,5 +1,9 @@
 package xialinHub.com.module.system.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 import xialinHub.com.platform.controller.BaseController;
 
 
@@ -8,6 +12,14 @@ import xialinHub.com.platform.controller.BaseController;
  *日期:2017年5月8日
  *时间:下午4:23:31
  **/
+@Controller
+@RequestMapping("system")
 public class SystemController extends BaseController {
 
+	@RequestMapping("index")
+	public ModelAndView index(){
+		ModelAndView mv = this.getModelAndView();
+		mv.setViewName("/module/system/role/roleindex");
+		return mv;
+	}
 }
--- xialinHub/xl_ktv_store/src/main/java/xialinHub/com/platform/controller/BaseController.java
+++ xialinHub/xl_ktv_store/src/main/java/xialinHub/com/platform/controller/BaseController.java
@@ -1,9 +1,16 @@
 package xialinHub.com.platform.controller;
+
+import org.springframework.web.servlet.ModelAndView;
 /*
  *作者:xialin
  *日期:2017年5月12日
  *时间:上午11:54:25
  **/
 public class BaseController {
+	
+	public ModelAndView getModelAndView(){
+		ModelAndView mv = new ModelAndView();
+		return mv;
+	}
 
 }
